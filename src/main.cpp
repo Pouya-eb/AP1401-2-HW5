2ce44b26d5ed0bba0e75df71c6a9cf8bdbdf2ac4
Pouya-eb <Pouyaebrahimi1379@gmail.com>
2023-05-28 20:22:58 +0330

    Updating main and unit_test

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include "../include/graph.h"
 #include "gtest/gtest.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     if (false) // make false to run unit tests
     {
         // debug section
-    }
-    else {
+    } else {
         ::testing::InitGoogleTest(&argc, argv);
         std::cout << RUN_ALL_TESTS() << std::endl;
     }
